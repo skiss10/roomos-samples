2ecd78844c3c481a560f00d59db7ba07148b283d
skiss10 <31713441+skiss10@users.noreply.github.com>
2024-02-01 15:42:56 -0500

    make me default

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -63,7 +63,7 @@ const dataModel = {
   },
 
   call() {
-    const defaultNumber = 'admin@stkiss-virtualreceptionist-sbx.calls.webex.com';
+    const defaultNumber = 'stkiss@cisco.com';
     const number = new URLSearchParams(location.search).get('reception') || defaultNumber;
     location.href = `sip:${number}`;
   },
